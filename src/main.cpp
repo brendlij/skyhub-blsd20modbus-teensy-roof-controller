11f428ad5d3a7649424ccd1afadeeebb165997e9
Julian Brendlin <194359046+brendlij@users.noreply.github.com>
2026-07-12 19:59:42 +0200

    Initial PlatformIO scaffold for Teensy 4.1
    Basic Arduino framework setup with LED blink and serial heartbeat.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  Serial.println("SkyHub BLSD20 Teensy Controller alive");
+}
